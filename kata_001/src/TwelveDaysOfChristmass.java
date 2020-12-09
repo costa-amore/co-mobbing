6799cf52c9b93f967efe03911017f0f1b47292e6
olivier <olivier.costa@aegisoft.be>
2020-12-09 21:44:20 +0100

    added behavior for day 2, started adding for day 3

--- kata_001/src/TwelveDaysOfChristmass.java
+++ kata_001/src/TwelveDaysOfChristmass.java
@@ -1,20 +1,43 @@
 public class TwelveDaysOfChristmass {
 
+    public static final String FINAL_SENTENCE_ALL_DAYS = "A partridge in a pear tree.";
+
     public static String giveMeYourLyrics(int day, int sentence) {
         if (sentence == 1) return "On the " + numberOfDay(day) + " day of Christmas";
         if (sentence == 2) return "My true love gave to me:";
-        return "A partridge in a pear tree.";
+        if (sentence == 3) {
+            if (day == 2) return "Two turtle doves and";
+            if (day == 3) return "Three french hens";
+        }
+        if (sentence == 4) {
+            if (day == 3) return "Two turtle doves and";
+        }
+        return FINAL_SENTENCE_ALL_DAYS;
     }
 
     public static String giveMeYourLyrics(int day) {
-        return  giveMeYourLyrics(1, 1)+"\n"+
-                giveMeYourLyrics(1, 2)+"\n"+
-                giveMeYourLyrics(1, 3)
+
+        String lyrics = giveMeYourLyrics(day, 1) + "\n" +
+                        giveMeYourLyrics(day, 2) + "\n" +
+                        giveMeYourLyrics(day, 3)
                 ;
+
+        if (day > 1) lyrics += "\n" + giveMeYourLyrics(day, 4);
+
+        return lyrics;
     }
 
     private static  String numberOfDay(int day) {
         if (day == 1) return "first";
-        return "second";
+        if (day == 2) return "second";
+        if (day == 3) return "third";
+        return "";
+    }
+
+    public static String giveMeYourLyrics() {
+        return giveMeYourLyrics(1) + "\n" +
+                "\n" +
+                giveMeYourLyrics(2)
+                ;
     }
 }
--- kata_001/src/TwelveDaysOfChristmassBehavior.java
+++ kata_001/src/TwelveDaysOfChristmassBehavior.java
@@ -13,22 +13,22 @@ public class TwelveDaysOfChristmassBehavior {
     @Nested
     public class Day1 {
         @Test
-        void of_first_day_first_sentence_is_generated() {
+        void first_sentence_is_generated() {
             assertEquals("On the first day of Christmas", TwelveDaysOfChristmass.giveMeYourLyrics(1, 1));
         }
 
         @Test
-        void of_first_day_second_sentence_is_generated() {
+        void second_sentence_is_always_the_same() {
             assertEquals("My true love gave to me:", TwelveDaysOfChristmass.giveMeYourLyrics(1, 2));
         }
 
         @Test
-        void of_first_day_third_sentence_is_generated() {
+        void third_sentence_is_generated() {
             assertEquals("A partridge in a pear tree.", TwelveDaysOfChristmass.giveMeYourLyrics(1, 3));
         }
 
         @Test
-        void of_first_day_all_sentences_are_generated() {
+        void all_sentences_are_generated() {
             assertEquals("On the first day of Christmas\n" +
                     "My true love gave to me:\n" +
                     "A partridge in a pear tree.", TwelveDaysOfChristmass.giveMeYourLyrics(1));
@@ -38,13 +38,59 @@ public class TwelveDaysOfChristmassBehavior {
     @Nested
     public class Day2 {
         @Test
-        void of_second_day_first_sentence_is_generated() {
+        void first_sentence_is_generated() {
             assertEquals("On the second day of Christmas", TwelveDaysOfChristmass.giveMeYourLyrics(2, 1));
+        }
+        @Test
+        void second_sentence_is_always_the_same() {
+            assertEquals("My true love gave to me:", TwelveDaysOfChristmass.giveMeYourLyrics(2, 2));
+        }
+        @Test
+        void third_sentence_is_generated() {
+            assertEquals("Two turtle doves and", TwelveDaysOfChristmass.giveMeYourLyrics(2, 3));
+        }
+        @Test
+        void fourth_sentence_is_generated() {
+            assertEquals("A partridge in a pear tree.", TwelveDaysOfChristmass.giveMeYourLyrics(2, 4));
+        }
+        @Test
+        void all_sentences_are_generated() {
+            assertEquals("On the second day of Christmas\n" +
+                    "My true love gave to me:\n" +
+                    "Two turtle doves and\n" +
+                    "A partridge in a pear tree.", TwelveDaysOfChristmass.giveMeYourLyrics(2));
+        }
+    }
+    @Nested
+    public class Day3 {
+        @Test
+        void first_sentence_is_generated() {
+            assertEquals("On the third day of Christmas", TwelveDaysOfChristmass.giveMeYourLyrics(3, 1));
+        }
+        @Test
+        void second_sentence_is_always_the_same() {
+            assertEquals("My true love gave to me:", TwelveDaysOfChristmass.giveMeYourLyrics(3, 2));
+        }
+        @Test
+        void third_sentence_is_generated() {
+            assertEquals("Three french hens", TwelveDaysOfChristmass.giveMeYourLyrics(3, 3));
+        }
+        @Test
+        void fourth_sentence_is_generated() {
+            assertEquals("Two turtle doves and", TwelveDaysOfChristmass.giveMeYourLyrics(3, 4));
         }
     }
 
-    @Nested
-    public class AllDays {
 
+    @Test
+    void all_days_are_generated() {
+        assertEquals("On the first day of Christmas\n" +
+                "My true love gave to me:\n" +
+                "A partridge in a pear tree.\n"+
+                "\n"+
+                "On the second day of Christmas\n" +
+                "My true love gave to me:\n" +
+                "Two turtle doves and\n" +
+                "A partridge in a pear tree.", TwelveDaysOfChristmass.giveMeYourLyrics());
     }
 }
