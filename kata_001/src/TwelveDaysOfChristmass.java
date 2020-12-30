1d1aecd60e1c8d97435f2b353d19898ddb171192
JurgenDS <jurgends@gmail.com>
2020-12-30 21:38:48 +0100

    finalised all 12 days and started comparing text output with challenge.txt content to see if we truly matched the exact challgen lyrics. Troubles started.

--- kata_001/src/TwelveDaysOfChristmass.java
+++ kata_001/src/TwelveDaysOfChristmass.java
@@ -5,13 +5,21 @@ public class TwelveDaysOfChristmass {
     public static String giveMeYourLyrics(int day, int sentence) {
         if (sentence == 1) return "On the " + numberOfDay(day) + " day of Christmas";
         if (sentence == 2) return "My true love gave to me:";
-        if (sentence == 3) {
-            if (day == 2) return "Two turtle doves and";
-            if (day == 3) return "Three french hens";
-        }
-        if (sentence == 4) {
-            if (day == 3) return "Two turtle doves and";
-        }
+
+        int delta = sentence-day;
+
+        if (delta == 1) return "Two turtle doves and";
+        if (delta == 0) return "Three french hens";
+        if (delta == -1) return "Four calling birds";
+        if (delta == -2) return "Five golden rings";
+        if (delta == -3) return "Six geese a-laying";
+        if (delta == -4) return "Seven swans a-swimming";
+        if (delta == -5) return "Eight maids a-milking";
+        if (delta == -6) return "Nine ladies dancing";
+        if (delta == -7) return "Ten lords a-leaping";
+        if (delta == -8) return "Eleven pipers piping";
+        if (delta == -9) return "Twelve drummers drumming";
+
         return FINAL_SENTENCE_ALL_DAYS;
     }
 
@@ -24,21 +32,48 @@ public class TwelveDaysOfChristmass {
 
         if (day > 1) lyrics += "\n" + giveMeYourLyrics(day, 4);
         if (day > 2) lyrics += "\n" + giveMeYourLyrics(day, 5);
+        if (day > 3) lyrics += "\n" + giveMeYourLyrics(day, 6);
+        if (day > 4) lyrics += "\n" + giveMeYourLyrics(day, 7);
+        if (day > 5) lyrics += "\n" + giveMeYourLyrics(day, 8);
+        if (day > 6) lyrics += "\n" + giveMeYourLyrics(day, 9);
+        if (day > 7) lyrics += "\n" + giveMeYourLyrics(day, 10);
+        if (day > 8) lyrics += "\n" + giveMeYourLyrics(day, 11);
+        if (day > 9) lyrics += "\n" + giveMeYourLyrics(day, 12);
+        if (day > 10) lyrics += "\n" + giveMeYourLyrics(day, 13);
+        if (day > 11) lyrics += "\n" + giveMeYourLyrics(day, 14);
+
 
         return lyrics;
+    }
+
+    public static String giveMeYourLyrics() {
+        return giveMeYourLyrics(1) + "\n" +
+                "\n" +
+                giveMeYourLyrics(2) + "\n" +
+                "\n" +
+                giveMeYourLyrics(3) + "\n" +
+                "\n" +
+                giveMeYourLyrics(4) + "\n" +
+                "\n" +
+                giveMeYourLyrics(5) + "\n" +
+                "\n" +
+                giveMeYourLyrics(6)
+        ;
     }
 
     private static  String numberOfDay(int day) {
         if (day == 1) return "first";
         if (day == 2) return "second";
         if (day == 3) return "third";
+        if (day == 4) return "forth";
+        if (day == 5) return "fifth";
+        if (day == 6) return "sixth";
+        if (day == 7) return "seventh";
+        if (day == 8) return "eight";
+        if (day == 9) return "ninth";
+        if (day == 10) return "tenth";
+        if (day == 11) return "eleventh";
+        if (day == 12) return "Twelfth";
         return "";
-    }
-
-    public static String giveMeYourLyrics() {
-        return giveMeYourLyrics(1) + "\n" +
-                "\n" +
-                giveMeYourLyrics(2)
-                ;
     }
 }
--- kata_001/src/TwelveDaysOfChristmassBehavior.java
+++ kata_001/src/TwelveDaysOfChristmassBehavior.java
@@ -1,11 +1,16 @@
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import sun.awt.HKSCS;
+
+import java.io.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TwelveDaysOfChristmassBehavior {
 
-    public static final int DAYS_SUPPORTED = 3;
+    public static final int DAYS_SUPPORTED = 12;
 
     @Nested
     public class Day1 {
@@ -18,13 +23,6 @@ public class TwelveDaysOfChristmassBehavior {
         void first_sentence_3_is_inserted() {
             assertEquals("A partridge in a pear tree.", TwelveDaysOfChristmass.giveMeYourLyrics(1, 3));
         }
-
-        @Test
-        void all_sentences_are_generated() {
-            assertEquals("On the first day of Christmas\n" +
-                    "My true love gave to me:\n" +
-                    "A partridge in a pear tree.", TwelveDaysOfChristmass.giveMeYourLyrics(1));
-        }
     }
     @Nested
     public class Day2 {
@@ -35,13 +33,6 @@ public class TwelveDaysOfChristmassBehavior {
         @Test
         void new_sentence_3_is_inserted() {
             assertEquals("Two turtle doves and", TwelveDaysOfChristmass.giveMeYourLyrics(2, 3));
-        }
-        @Test
-        void all_sentences_are_generated() {
-            assertEquals("On the second day of Christmas\n" +
-                    "My true love gave to me:\n" +
-                    "Two turtle doves and\n" +
-                    "A partridge in a pear tree.", TwelveDaysOfChristmass.giveMeYourLyrics(2));
         }
     }
     @Nested
@@ -54,26 +45,126 @@ public class TwelveDaysOfChristmassBehavior {
         void new_sentence_3_is_inserted() {
             assertEquals("Three french hens", TwelveDaysOfChristmass.giveMeYourLyrics(3, 3));
         }
+    }
+    @Nested
+    public class Day4 {
         @Test
-        void all_sentences_are_generated() {
-            assertEquals("On the third day of Christmas\n" +
+        void sentence_1_is_generated() {
+            assertEquals("On the forth day of Christmas", TwelveDaysOfChristmass.giveMeYourLyrics(4, 1));
+        }
+        @Test
+        void new_sentence_3_is_inserted() {
+            assertEquals("Four calling birds", TwelveDaysOfChristmass.giveMeYourLyrics(4, 3));
+        }
+        @Test
+        void full_lyrics_day_four() {
+            assertEquals( "On the forth day of Christmas\n" +
                     "My true love gave to me:\n" +
+                    "Four calling birds\n" +
                     "Three french hens\n" +
                     "Two turtle doves and\n" +
-                    "A partridge in a pear tree.", TwelveDaysOfChristmass.giveMeYourLyrics(3));
+                    "A partridge in a pear tree.", TwelveDaysOfChristmass.giveMeYourLyrics(4));
+        }
+    }
+    @Nested
+    public class Day5 {
+        @Test
+        void sentence_1_is_generated() {
+            assertEquals("On the fifth day of Christmas", TwelveDaysOfChristmass.giveMeYourLyrics(5, 1));
+        }
+        @Test
+        void new_sentence_3_is_inserted() {
+            assertEquals("Five golden rings", TwelveDaysOfChristmass.giveMeYourLyrics(5, 3));
+        }
+    }
+    @Nested
+    public class Day6 {
+        @Test
+        void sentence_1_is_generated() {
+            assertEquals("On the sixth day of Christmas", TwelveDaysOfChristmass.giveMeYourLyrics(6, 1));
+        }
+        @Test
+        void new_sentence_3_is_inserted() {
+            assertEquals("Six geese a-laying", TwelveDaysOfChristmass.giveMeYourLyrics(6, 3));
+        }
+    }
+    @Nested
+    public class Day7 {
+        @Test
+        void sentence_1_is_generated() {
+            assertEquals("On the seventh day of Christmas", TwelveDaysOfChristmass.giveMeYourLyrics(7, 1));
+        }
+        @Test
+        void new_sentence_3_is_inserted() {
+            assertEquals("Seven swans a-swimming", TwelveDaysOfChristmass.giveMeYourLyrics(7, 3));
+        }
+    }
+    @Nested
+    public class Day8 {
+        @Test
+        void sentence_1_is_generated() {
+            assertEquals("On the eight day of Christmas", TwelveDaysOfChristmass.giveMeYourLyrics(8, 1));
+        }
+        @Test
+        void new_sentence_3_is_inserted() {
+            assertEquals("Eight maids a-milking", TwelveDaysOfChristmass.giveMeYourLyrics(8, 3));
+        }
+    }
+    @Nested
+    public class Day9 {
+        @Test
+        void sentence_1_is_generated() {
+            assertEquals("On the ninth day of Christmas", TwelveDaysOfChristmass.giveMeYourLyrics(9, 1));
+        }
+        @Test
+        void new_sentence_3_is_inserted() {
+            assertEquals("Nine ladies dancing", TwelveDaysOfChristmass.giveMeYourLyrics(9, 3));
+        }
+    }
+    @Nested
+    public class Day10 {
+        @Test
+        void sentence_1_is_generated() {
+            assertEquals("On the tenth day of Christmas", TwelveDaysOfChristmass.giveMeYourLyrics(10, 1));
+        }
+        @Test
+        void new_sentence_3_is_inserted() {
+            assertEquals("Ten lords a-leaping", TwelveDaysOfChristmass.giveMeYourLyrics(10, 3));
+        }
+    }
+    @Nested
+    public class Day11 {
+        @Test
+        void sentence_1_is_generated() {
+            assertEquals("On the eleventh day of Christmas", TwelveDaysOfChristmass.giveMeYourLyrics(11, 1));
+        }
+        @Test
+        void new_sentence_3_is_inserted() {
+            assertEquals("Eleven pipers piping", TwelveDaysOfChristmass.giveMeYourLyrics(11, 3));
+        }
+    }
+    @Nested
+    public class Day12 {
+        @Test
+        void sentence_1_is_generated() {
+            assertEquals("On the Twelfth day of Christmas", TwelveDaysOfChristmass.giveMeYourLyrics(12, 1));
+        }
+        @Test
+        void new_sentence_3_is_inserted() {
+            assertEquals("Twelve drummers drumming", TwelveDaysOfChristmass.giveMeYourLyrics(12, 3));
         }
     }
 
-    @Test
-    void all_days_are_generated() {
-        assertEquals("On the first day of Christmas\n" +
-                "My true love gave to me:\n" +
-                "A partridge in a pear tree.\n"+
-                "\n"+
-                "On the second day of Christmas\n" +
-                "My true love gave to me:\n" +
-                "Two turtle doves and\n" +
-                "A partridge in a pear tree.", TwelveDaysOfChristmass.giveMeYourLyrics());
+    @Ignore // move challenge.txt to lycircs & compare line by line
+    void all_days_are_generated() throws IOException {
+        String challengeInput = new String("");
+        String sentenceRead;
+        File file = new File("/Users/jurgendesmet/Dev/co-mobbing/kata_001/src/challenge.txt");
+        BufferedReader bufferReader = new BufferedReader( new FileReader(file));
+        while ((sentenceRead = bufferReader.readLine()) != null) {
+            challengeInput = challengeInput.concat(sentenceRead + "\n");
+        }
+        assertTrue(challengeInput.contains(TwelveDaysOfChristmass.giveMeYourLyrics()));
     }
 
     @Test
